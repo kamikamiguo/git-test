b6cb1076f35d4f522cdb4866d5259745d6cb46f0
93665 <936657687@qq.com>
2021-09-24 17:04:02 +0800

    mast-commit

--- src/main/java/com/aiguigu/git/GitTest.java
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("mast-test!");
         System.out.println("hot-fix!");
+        System.out.println("pust-test!");
     }
 }
